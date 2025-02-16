003dc20bad6ab0d359210f851f3d43a371cbf812
TikhonovNikita1996 <nikita.tikonov1996@bk.ru>
2025-02-16 17:04:42 +0300

    Not found page, page loader, unit tests

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -1,20 +1,20 @@
-import {classNames} from "shared/lib/classNames/classNames";
-import cls from './AppLink.module.scss'
-import {Link, LinkProps} from "react-router-dom";
-import {FC} from "react";
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Link, LinkProps } from 'react-router-dom';
+import { FC } from 'react';
+import cls from './AppLink.module.scss';
 
 export enum AppLinkTheme {
     PRIMARY = 'primary',
     SECONDARY = 'secondary',
+    RED = 'red',
 }
 
-
-interface AppLinkProps extends  LinkProps{
+interface AppLinkProps extends LinkProps {
     className?: string;
-    theme?: AppLinkTheme
+    theme?: AppLinkTheme;
 }
 
-const AppLink: FC<AppLinkProps> = (props) => {
+export const AppLink: FC<AppLinkProps> = (props) => {
     const {
         to,
         className,
@@ -24,11 +24,12 @@ const AppLink: FC<AppLinkProps> = (props) => {
     } = props;
 
     return (
-        <Link to={to} className={classNames(cls.AppLink, {}, [className, cls[theme]])}
-        {...otherProps}>
+        <Link
+            to={to}
+            className={classNames(cls.AppLink, { [cls[theme]]: true }, [className])}
+            {...otherProps}
+        >
             {children}
         </Link>
     );
 };
-
-export default AppLink;
