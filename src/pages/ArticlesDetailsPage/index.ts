39e6b505a0cca8c6519420017e71b032c9c7b83d
TikhonovNikita1996 <nikita.tikonov1996@bk.ru>
2025-03-06 12:57:44 +0300

    Add comments list

--- src/pages/ArticlesDetailsPage/index.ts
+++ src/pages/ArticlesDetailsPage/index.ts
@@ -3,3 +3,5 @@ import { ArticleDetailsPageAsync } from './ui/ArticleDetailPage/ArticleDetailsPa
 export {
     ArticleDetailsPageAsync as ArticleDetailsPage,
 };
+
+export { ArticleDetailsCommentsSchema } from './model/types/ArticleDetailsCommentsSchema';
--- src/pages/ArticlesDetailsPage/ui/ArticleDetailPage/ArticleDetailsPage.tsx
+++ src/pages/ArticlesDetailsPage/ui/ArticleDetailPage/ArticleDetailsPage.tsx
@@ -3,7 +3,18 @@ import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
 import { ArticleDetails } from 'entities/Article';
 import { useParams } from 'react-router-dom';
+import { Text } from 'shared/ui/Text/Text';
+import { CommentList } from 'entities/Comment';
+import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { useSelector } from 'react-redux';
+import { getArticleCommentsIsLoading } from '../../model/selectors/comments';
 import cls from './ArticleDetailsPage.module.scss';
+import { articleDetailsCommentsReducer, getArticleComments } from '../../model/slices/articleDetailsCommentsSlice';
+import {useInitialEffect} from "shared/lib/hooks/useInitialEffect/useInitialEffect";
+import {useAppDispatch} from "shared/lib/hooks/useAppDispatch/useAppDispatch";
+import {
+    fetchCommentsByArticleId
+} from "pages/ArticlesDetailsPage/model/services/fetchCommentsByArticleId/fetchCommentsByArticleId";
 
 interface ArticleDetailsPageProps {
     className?: string;
@@ -14,6 +25,17 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
     const { className } = props;
     const { t } = useTranslation('article-details');
     const { id } = useParams<{id: string}>();
+    const dispatch = useAppDispatch();
+    const comments = useSelector(getArticleComments.selectAll);
+    const commentsIsLoading = useSelector(getArticleCommentsIsLoading);
+
+    const reducersList: ReducersList = {
+        articleDetailsComments: articleDetailsCommentsReducer,
+    };
+
+    useInitialEffect(() => {
+        dispatch(fetchCommentsByArticleId(id));
+    });
 
     if (!id) {
         return (
@@ -24,9 +46,16 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
     }
 
     return (
-        <div className={classNames(cls.ArticleDetailsPage, {}, [])}>
-            <ArticleDetails id={id} />
-        </div>
+        <DynamicModuleLoader reducers={reducersList} removeAfterUnmount>
+            <div className={classNames(cls.ArticleDetailsPage, {}, [])}>
+                <ArticleDetails id={id} />
+                <Text className={cls.commentTitle} title={t('Комментарии')} />
+                <CommentList
+                    isLoading={commentsIsLoading}
+                    comments={comments}
+                />
+            </div>
+        </DynamicModuleLoader>
     );
 };
 
