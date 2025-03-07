ae56ef639b979b0cb72c0d4950893ae81054d87b
TikhonovNikita1996 <nikita.tikonov1996@bk.ru>
2025-03-07 11:57:13 +0300

    Article list view selection

--- src/pages/ArticlesPage/index.ts
+++ src/pages/ArticlesPage/index.ts
@@ -3,3 +3,4 @@ import { ArticlesPageAsync } from './ui/ArticlesPage/ArticlesPage.async';
 export {
     ArticlesPageAsync as ArticlesPage,
 };
+export { ArticlesPageSchema } from './model/types/articlesPageSchema';
