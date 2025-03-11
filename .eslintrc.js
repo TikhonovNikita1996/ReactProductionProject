9608fe8be8901ba4de43c65cdbeae24c8ec84180
TikhonovNikita1996 <nikita.tikonov1996@bk.ru>
2025-03-11 11:47:28 +0300

    custom listbox added

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,7 +47,7 @@ module.exports = {
             {
                 markupOnly: true,
                 ignoreAttribute: ['data-testid', 'to', 'target', 'justify',
-                    'align', 'direction', 'gap', 'role'],
+                    'align', 'direction', 'gap', 'role', 'as'],
             },
         ],
         'max-len': ['error', { ignoreComments: true, code: 120 }],
