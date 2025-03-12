7169d8e7f48566b24d74efd5be147ed5897692af
TikhonovNikita1996 <nikita.tikonov1996@bk.ru>
2025-03-12 15:52:10 +0300

    add popover, and notifications

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,7 +47,7 @@ module.exports = {
             {
                 markupOnly: true,
                 ignoreAttribute: ['data-testid', 'to', 'target', 'justify',
-                    'align', 'direction', 'gap', 'role', 'as'],
+                    'align', 'direction', 'gap', 'role', 'as', 'border'],
             },
         ],
         'max-len': ['error', { ignoreComments: true, code: 120 }],
